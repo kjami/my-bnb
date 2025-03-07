d32e446c84f2fee3fbf36ba6fd369f30402ae8d0
Kishor Jami <kishorjami@gmail.com>
2025-03-06 23:09:17 -0500

    Get search results based on search params

--- app/properties/search-results/page.js
+++ app/properties/search-results/page.js
@@ -1,4 +1,31 @@
-const SearchResultsPage = () => {
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { convertToSerializableObject } from "@/utils/convert-to-object";
+
+const SearchResultsPage = async ({ searchParams: { location, proprtyType }}) => {
+    await connectDB();
+
+    const locationPattern = new RegExp(location, "i");
+
+    const query = {
+        $or: [
+            { name: locationPattern },
+            { description: locationPattern },
+            { "location.street": locationPattern },
+            { "location.city": locationPattern },
+            { "location.state": locationPattern },
+            { "location.zipcode": locationPattern },
+        ]
+    };
+
+    if (proprtyType && proprtyType !== "All") {
+        query.type = new RegExp(proprtyType, "i");
+    }
+
+    const res = await Property.find(query).lean();
+    const properties = convertToSerializableObject(res);
+    console.log(properties);
+
     return (<div>Search Results Page!</div>)
 };
 
